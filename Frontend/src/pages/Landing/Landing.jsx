4d6d8310d8535950b5fb603b9ea1dee738eaaa6a
rajani <rajanikushwaha@gmail.com>
2026-08-09 21:50:15 +0530

    Added frontent

---
+++ Frontend/src/pages/Landing/Landing.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../../components/layout/Navbar";
+import Hero from "../../components/home/Hero";
+import Stats from "../../components/home/Stats";
+import Features from "../../components/home/Features";
+
+const Landing = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Stats />
+      <Features />
+    </>
+  );
+};
+
+export default Landing;
