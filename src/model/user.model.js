ba90d1d8f9928f206b7c6f40a486171bda38a593
Lalit Kumar <prajapatilalitkumar263@gmail.com>
2026-08-11 21:20:40 +0530

    Authication System is Done

---
+++ src/model/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+const UserSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    uid:{
+        type:String,
+        required:true``
+    },
+    phoneUrl:{
+        type:String,
+        default:""
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:['Admin','Student'],
+        default:'Student',
+        required:true
+    }
+},
+{
+     timestamps: true
+}
+)
+const Auth=mongoose.model('Auth',authSchema)
+export default Auth
---
+++ src/routes/user.routes.js
@@ -0,0 +1,7 @@
+import express from 'express'
+import { createUser, getCurrentUser, logout } from '../controller/auth.controller.js'
+const authRouter=express.Router()
+authRouter.post("/create-user",createUser)
+authRouter.post("/logout",logout)
+authRouter.get("/me",verifyAuth,getCurrentUser)
+export default authRouter
