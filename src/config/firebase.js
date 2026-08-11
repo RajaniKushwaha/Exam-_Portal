a3e5c20a244b0acbc8be7ee4554419d4e6ec105c
ankul-in <ankulin@outlook.com>
2026-08-11 22:24:21 +0530

    feat: set up project structure with Firebase Admin and MongoDB

--- src/config/firebase.js
+++
@@ -1,6 +0,0 @@
-import admin from "firebase-admin"
-import serviceAccount from "./firebase.json"
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-export default admin
